2e77f4e2bb66828f4875ed14fdb657ad9c6819e8
IMBOT-INC <master@i-mbot.info>
2026-08-26 17:10:15 +0900

    ✨ Feat(UI): 퀘스트 추적 위젯 C++ 파일 추가

---
+++ Source/TD_Project/UI/HUD/TDQuestTrackerWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TDQuestTrackerWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable)
+class TD_PROJECT_API UTDQuestTrackerWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};

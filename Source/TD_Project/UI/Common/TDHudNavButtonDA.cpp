f4887c5b6b21aad639bb528b0e5c525bce74c013
IMBOT-INC <master@i-mbot.info>
2026-08-26 17:09:39 +0900

    ✨ Feat(UI): HUD 네비게이션 버튼 스타일 데이터 에셋 추가

---
+++ Source/TD_Project/UI/Common/TDHudNavButtonDA.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TDHudNavButtonDA.h"
+
+FButtonStyle UTDHudNavButtonDA::GetStyle(bool bSelected) const
+{
+	if (!bSelected)
+	{
+		return DefaultStyle;
+	}
+
+	FButtonStyle Result = DefaultStyle;
+
+	// 선택된 동안 호버하거나 눌러도 선택 이미지 유지
+	Result.SetNormal(SelectedBrush);
+	Result.SetHovered(SelectedBrush);
+	Result.SetPressed(SelectedBrush);
+
+	return Result;
+	
+}
---
+++ Source/TD_Project/UI/Common/TDHudNavButtonDA.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "TDHudNavButtonDA.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TD_PROJECT_API UTDHudNavButtonDA : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+	// Normal, Hovered, Pressed, Disabled를 포함하는 기본 버튼 스타일
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Button Style")
+		FButtonStyle DefaultStyle;
+
+	// 버튼이 선택된 상태에서 사용할 이미지
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Button Style")
+		FSlateBrush SelectedBrush;
+
+	// 선택 여부에 맞는 완성된 버튼 스타일 반환
+	UFUNCTION(BlueprintPure, Category = "Button Style")
+		FButtonStyle GetStyle(bool bSelected) const;
+	
+};
